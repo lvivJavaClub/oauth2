0113ae7cfbebf1d304dcd874cd02834b7a26541c
Serge Bishyr <seriybg@gmail.com>
2018-03-29 10:34:07 +0300

    add file data store

--- cli/src/main/java/com/lohika/jclub/oauth2/Oauth2CliApp.java
+++ cli/src/main/java/com/lohika/jclub/oauth2/Oauth2CliApp.java
@@ -1,5 +1,6 @@
 package com.lohika.jclub.oauth2;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
@@ -14,7 +15,7 @@ import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.DataStoreFactory;
-import com.google.api.client.util.store.MemoryDataStoreFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.oauth2.Oauth2;
 import com.google.api.services.oauth2.model.Tokeninfo;
 import com.google.api.services.oauth2.model.Userinfoplus;
@@ -25,12 +26,14 @@ public class Oauth2CliApp {
             "https://www.googleapis.com/auth/userinfo.email",
             "https://www.googleapis.com/auth/userinfo.profile");
 
+    private static final File file = new File(System.getProperty("user.home") + ".store/");
+
     public static void main(String[] args) throws IOException, GeneralSecurityException {
         NetHttpTransport netHttpTransport = GoogleNetHttpTransport.newTrustedTransport();
         JacksonFactory jsonFactory = JacksonFactory.getDefaultInstance();
         GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(jsonFactory,
                 new InputStreamReader(Oauth2CliApp.class.getResourceAsStream("/client_secrets.json")));
-        DataStoreFactory dataStoreFactory = new MemoryDataStoreFactory();
+        DataStoreFactory dataStoreFactory = new FileDataStoreFactory(file);
 
         GoogleAuthorizationCodeFlow flow =
                 new GoogleAuthorizationCodeFlow.Builder(netHttpTransport, jsonFactory, clientSecrets.getDetails().getClientId(), clientSecrets.getDetails().getClientSecret(), SCOPES)
@@ -39,6 +42,7 @@ public class Oauth2CliApp {
         Credential credential = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
         Oauth2 oauth2 = new Oauth2.Builder(netHttpTransport, jsonFactory, credential).setApplicationName("").build();
 
+        System.out.println(credential.getAccessToken());
         Tokeninfo tokeninfo = oauth2.tokeninfo().setAccessToken(credential.getAccessToken()).execute();
         System.out.println(tokeninfo.toPrettyString());
         Userinfoplus userinfoplus = oauth2.userinfo().get().execute();
